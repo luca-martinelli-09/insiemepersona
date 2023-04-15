e42b089bc49a2f73728b3162f211772303fb65fe
Luca Martinelli <martinelliluca98@gmail.com>
2023-04-15 17:06:00 +0200

    Generatore schede

--- generate-schede.py
+++ generate-schede.py
@@ -1,4 +1,35 @@
-import base64
+# sudo apt install inkscape
 
-with open("static/cv/luca-martinelli/profile.jpeg", "rb") as img_file:
-    my_string = base64.b64encode(img_file.read())
+# %%
+import os
+import base64
+import pandas as pd
+import subprocess
+
+candidati = pd.read_csv('candidati.csv', index_col=['id'])
+
+for candidatoID, data in candidati.iterrows():
+    print(f"[🤖 GENERANDO] {data['nome']} {data['cognome']}")
+
+    template = open(f"schede-candidati/scheda-{data['lista']}.svg").read()
+
+    imageLink = ""
+    imagePath = f"static/cv/{candidatoID}/{candidatoID}.jpeg"
+    if os.path.exists(imagePath):
+        with open(imagePath, "rb") as fp:
+            imageData = base64.b64encode(fp.read())
+            imageLink = f"data:image/jpeg;base64,{imageData.decode()}"
+
+    savePathSVG = f"schede-candidati/svg/{candidatoID}.svg"
+    savePathPNG = f"schede-candidati/png/{candidatoID}.png"
+
+    with open(savePathSVG, "w+") as fp:
+        template = template.replace("[[NOME]]", data['nome'])
+        template = template.replace("[[COGNOME]]", data['cognome'])
+        template = template.replace("[[FOTO_PROFILO]]", imageLink)
+
+        fp.write(template)
+
+    subprocess.run(("inkscape", savePathSVG, "-o", savePathPNG))
+
+# %%
